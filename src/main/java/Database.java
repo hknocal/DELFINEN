5464edd5653edb4e508af609a6abca33e7c06db7
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-11-22 17:30:24 +0100

    Made an arraylist and a add method to the database class

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,3 +1,16 @@
-public class Database {
+import java.util.ArrayList;
 
+public class Database {
+    private ArrayList <Member> memberArrayList = new ArrayList<>();
+
+    public ArrayList <Member> getMemberArrayList(){ //Get metode to our arraylist
+        return memberArrayList;
+    }
+
+    //Add method to our arraylist
+    public void addMembers(String name, int date, int month, int year, int phoneNumber, String eMail, boolean activityStatus, int memberNr){
+        Member member = new Member(name, date, month, year, phoneNumber, eMail, activityStatus, memberNr);
+        memberArrayList.add(member);
+    }
 }
+
